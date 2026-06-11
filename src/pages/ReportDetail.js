adb475ce6d11cbb21a761c49d02f71edc75c77ad
Manas Naik <manasnaik0704@gmail.com>
2026-06-11 11:32:45 +0530

    Added ReportDetail page

---
+++ src/pages/ReportDetail.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ReportDetail() {
+
+    return (
+
+        <div className="p-8">
+
+            <h1 className="text-3xl font-bold">
+
+                Report Detail Page
+
+            </h1>
+
+        </div>
+
+    );
+
+}
+
+export default ReportDetail;
